910b3352bfc3387652c322ead71e770b6bea01de
Oscar Orellana <oscar@softcorp.io>
2020-12-16 11:26:17 +0100

    refactoring2

--- hqs_user_service/dart/hqs_user_service.pb.dart
+++ hqs_user_service/dart/hqs_user_service.pb.dart
@@ -1,6 +1,6 @@
 ///
 //  Generated code. Do not modify.
-//  source: hqs_user_service/hqs_user_service.proto
+//  source: hqs_user_service.proto
 //
 // @dart = 2.3
 // ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields
@@ -9,7 +9,7 @@ import 'dart:core' as $core;
 
 import 'package:protobuf/protobuf.dart' as $pb;
 
-import '../google/protobuf/timestamp.pb.dart' as $1;
+import 'google/protobuf/timestamp.pb.dart' as $1;
 
 class User extends $pb.GeneratedMessage {
   static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'User', package: const $pb.PackageName(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'UserService'), createEmptyInstance: create)
